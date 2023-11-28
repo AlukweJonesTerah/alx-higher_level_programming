0db0ab62e2498f814344738cf57b8a982052b4cc
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-11-28 14:14:15 -0800

    fifthy commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,12 @@ if number >= 0:
     elif number % 10 > 5:
         print(f'Last digit of {number} is {number % 10} and is greater than 5')
     else:
-        print(f'Last digit of {number} is {number % 10} and is less than 6 and not 0')
+        print(f'Last digit of {number} is {number % 10} and is less than 6 and
+                not 0')
 else:
     if number % -10 == 0:
         print(f'Last digit of {number} is {number % 10} and is 0')
     else:
-        print(f'Last digit of {number} is {number % -10} and is'\ 
-                'less than 6 and not 0')
+        print(af'Last digit of {number} is {number % -10} and is less than 6 and
+                not 0')
 
