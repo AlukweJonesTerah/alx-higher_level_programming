16ce882360d97a2a226f4826c1b8cf6a95c17585
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-11-30 20:48:54 +0300

    commmit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-cal = __import__('calculator_1')
 if __name__ == "__main__":
     a = 10
     b = 5
+    cal = __import__('calculator_1')
     print('{} + {} = {}'.format(a, b, cal.add(a, b)))
     print('{} - {} = {}'.format(a, b, cal.sub(a, b)))
     print('{} * {} = {}'.format(a, b, cal.mul(a, b)))
