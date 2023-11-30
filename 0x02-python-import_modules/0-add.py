9f72f0134f52f26a4cf2d44e42f4f9b2f4c4b11d
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-11-30 20:35:22 +0300

    third commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 import add_0
 
 if __name__ == "__main__":
     import sys
     a = 1
     b = 2
-    c = add(a, b)
+    c = add_0.add(a, b)
     print('{} + {} = {}'.format(a, b, c))
