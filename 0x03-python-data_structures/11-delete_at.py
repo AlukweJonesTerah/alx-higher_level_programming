e394855c2978c6a50fc2472f50e8a2e01d356dd1
AlukweJonesTerah <jtalukwe@kabarak.ac.ke>
2023-12-04 15:40:48 +0300

    commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = my_list[:idx] + my_list[idx+1:]
+    del my_list[idx]
+    new_list = my_list
     return new_list
